f8b9bcb1948cf8d5e53000a017f7af0d6aab1459
Bruno Polaco Penteado <polaco@gmail.com>
2013-01-26 00:46:48 -0200

    Improving the display when listing all polls on admin

--- tutorial/polls/admin.py
+++ tutorial/polls/admin.py
@@ -8,6 +8,8 @@ class ChoiceInline(admin.TabularInline):
 
 
 class PollAdmin(admin.ModelAdmin):
+    list_display = ('question', 'pub_date', 'was_published_recently')
+
     fieldsets = [
         (None,               {'fields': ['question']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
