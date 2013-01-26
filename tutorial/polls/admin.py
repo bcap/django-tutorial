41d87776e45a149a7f94b3b1477d8d833cc3b9d0
Bruno Polaco Penteado <polaco@gmail.com>
2013-01-26 00:38:54 -0200

    Adding fieldset class

--- tutorial/polls/admin.py
+++ tutorial/polls/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 class PollAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question']}),
-        ('Date information', {'fields': ['pub_date']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
 
 
